1b9e6499bd20e46809d9498ea65c704da85632fa
Pavel <legenda1207@gmail.com>
2021-02-26 11:08:03 +0200

    start socket

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,39 @@
 import app from "./app";
+import http from "http";
 import { loadDB } from "./database";
+const socketIo = require("socket.io");
 
 const PORT = process.env.PORT || 1328;
 
 loadDB();
 
-app.listen(PORT, () => console.log(`running on port http://localhost:${PORT}`));
+const server = http.createServer(app.callback());
+
+server.listen(PORT, () =>
+  console.log(`running on port http://localhost:${PORT}`)
+);
+
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+let interval;
+
+io.on("connection", (socket) => {
+  console.log("New client connected");
+  if (interval) {
+    clearInterval(interval);
+  }
+  interval = setInterval(() => getApiAndEmit(socket), 1000);
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
+    clearInterval(interval);
+  });
+});
+
+const getApiAndEmit = (socket) => {
+  const response = new Date();
+  socket.emit("FromAPI", response);
+};
