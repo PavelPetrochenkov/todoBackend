350529cf4d96def80a9974c06a268e00d6c1c972
Pavel <legenda1207@gmail.com>
2021-03-01 12:45:25 +0200

    add auth socket

--- src/api/controllers/index.ts
+++ src/api/controllers/index.ts
@@ -1,12 +1,10 @@
 import Router from "koa-router";
-import configPrivateRouter from "./private";
 import configPublicRouter from "./public";
 
 export default function configAllRouter() {
   const router: Router = new Router({ prefix: "/api" });
 
   router.use(configPublicRouter());
-  router.use(configPrivateRouter());
 
   return router.routes();
 }
--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
-import app from './app';
+import app from "./app";
 import { ObjectId } from "mongodb";
-import http from 'http';
-import { loadDB } from './database';
+import jwt_decode from "jwt-decode";
+import http from "http";
+import { loadDB } from "./database";
 import {
   addTodo,
   checkAllTodos,
@@ -9,14 +10,11 @@ import {
   deleteTodo,
   getTodos,
   updateTodo,
-} from './socket';
-import { verifyToken,
-  createToken,
-  createRefreshToken, } from './jwt/jwt';
-const socketIo = require('socket.io');
-import jwt_decode from "jwt-decode";
+} from "./socket";
+import { verifyToken, createToken, createRefreshToken } from "./jwt/jwt";
 import refreshTokensCollection from "./api/models/refreshTokensModels";
-import userCollection from "./api/models/userModels";
+
+const socketIo = require("socket.io");
 
 const PORT = process.env.PORT || 1328;
 
@@ -24,102 +22,193 @@ loadDB();
 
 const server = http.createServer(app.callback());
 
-server.listen(PORT, () => console.log(`running on port http://localhost:${PORT}`));
+server.listen(PORT, () =>
+  console.log(`running on port http://localhost:${PORT}`)
+);
 
 const io = socketIo(server, {
   cors: {
-    origin: '*',
+    origin: "*",
   },
 });
 
 io.use((socket, next) => {
   try {
-    verifyToken(socket.handshake.headers.token);
+    checkAuth(
+      socket.handshake.headers.token,
+      socket.handshake.headers.refreshtoken,
+      socket,
+      () => {}
+    );
     next();
   } catch (err) {
     return err;
   }
 });
 
-io.on('connection', (socket) => {
-  console.log('New client connected');
-  socket.join('Socket:' + socket.handshake.headers.userid);
+io.on("connection", (socket) => {
+  console.log("New client connected");
 
   const userId = socket.handshake.headers.userid;
+  const userGroup = "Socket:" + userId;
 
-  socket.on('getTodos', async (userId: string) => {
-    const todos = await getTodos(userId);
-    io.to('Socket:' + socket.handshake.headers.userid).emit('getTodosSuccess', todos);
+  socket.join(userGroup);
+
+  socket.on("getTodos", async (userId: string) => {
+    checkAuth(
+      socket.handshake.headers.token,
+      socket.handshake.headers.refreshtoken,
+      socket,
+      async () => {
+        const todos = await getTodos(userId);
+        io.to(userGroup).emit("getTodosSuccess", todos);
+      }
+    );
   });
 
-  socket.on('addTodo', async (text: string) => {
-    const todo = await addTodo(text, userId);
-    io.to('Socket:' + socket.handshake.headers.userid).emit('addTodoSuccess', todo);
+  socket.on("addTodo", async (text: string) => {
+    checkAuth(
+      socket.handshake.headers.token,
+      socket.handshake.headers.refreshtoken,
+      socket,
+      async () => {
+        const todo = await addTodo(text, userId);
+        io.to(userGroup).emit("addTodoSuccess", todo);
+      }
+    );
   });
 
-  socket.on('deleteTodo', async (id: string) => {
-    await deleteTodo(id);
-    io.to('Socket:' + socket.handshake.headers.userid).emit('deleteTodoSuccess', id);
+  socket.on("deleteTodo", async (id: string) => {
+    checkAuth(
+      socket.handshake.headers.token,
+      socket.handshake.headers.refreshtoken,
+      socket,
+      async () => {
+        await deleteTodo(id);
+        io.to(userGroup).emit("deleteTodoSuccess", id);
+      }
+    );
   });
 
-  socket.on('changeTodo', async (id: string, userId: string, opt) => {
-    let todo;
+  socket.on(
+    "changeTodo",
+    async (id: string, userId: string, opt: string | boolean) => {
+      checkAuth(
+        socket.handshake.headers.token,
+        socket.handshake.headers.refreshtoken,
+        socket,
+        async () => {
+          let todo;
 
-    if (typeof opt === 'boolean') {
-      todo = await updateTodo({ id, userId, check: opt });
-    } else {
-      todo = await updateTodo({ id, userId, text: opt });
+          if (typeof opt === "boolean") {
+            todo = await updateTodo({ id, userId, check: opt });
+          } else {
+            todo = await updateTodo({ id, userId, text: opt });
+          }
+
+          io.to(userGroup).emit("changeTodoSuccess", todo);
+        }
+      );
     }
+  );
 
-    io.to('Socket:' + socket.handshake.headers.userid).emit('changeTodoSuccess', todo);
+  socket.on("checkAllTodos", async (userId: string, check: boolean) => {
+    checkAuth(
+      socket.handshake.headers.token,
+      socket.handshake.headers.refreshtoken,
+      socket,
+      async () => {
+        const todos = await checkAllTodos({ userId, check });
+
+        io.to(userGroup).emit("checkAllTodosSuccess", todos);
+      }
+    );
   });
 
-  socket.on('checkAllTodos', async (userId: string, check: boolean) => {
-    const todos = await checkAllTodos({ userId, check });
-
-    io.to('Socket:' + socket.handshake.headers.userid).emit('checkAllTodosSuccess', todos);
+  socket.on("clearAllCompletedTodos", async (userId: string) => {
+    checkAuth(
+      socket.handshake.headers.token,
+      socket.handshake.headers.refreshtoken,
+      socket,
+      async () => {
+        const todos = await deleteCompletedTodos(userId);
+        io.to(userGroup).emit("clearAllCompletedTodosSuccess", todos);
+      }
+    );
   });
 
-  socket.on('clearAllCompletedTodos', async (userId: string) => {
-    const todos = await deleteCompletedTodos(userId);
-    io.to('Socket:' + socket.handshake.headers.userid).emit('clearAllCompletedTodosSuccess', todos);
-  });
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+  socket.on("disconnect", async () => {
+    await refreshTokensCollection.updateOne({
+      _id: ObjectId(userId),
+      refreshToken: "",
+    });
+    console.log("Client disconnected");
     socket.disconnect();
   });
 });
 
-const refreshToken = async (refreshToken) => {
+const checkAuth = async (
+  token: string,
+  refreshToken: string,
+  socket: any,
+  func: () => void
+) => {
+  const isAuth = await checkToken(token, refreshToken);
+
+  if (isAuth.error) {
+    await socket.emit("clearAuth");
+    socket.disconnect();
+    return;
+  }
+
+  await func();
+
+  if (!isAuth.verify) {
+    socket.emit("refreshToken", {
+      token: isAuth.token,
+      refreshToken: isAuth.refreshToken,
+    });
+  }
+};
+
+const checkToken = async (token: string, refreshToken: string) => {
   try {
-    const decodedToken: { id: string } = jwt_decode(refreshToken);
+    await verifyToken(token);
+    return { verify: true, error: false, token: "", refreshToken: "" };
+  } catch (err) {
+    const res = await checkRefreshToken(refreshToken);
+    return res;
+  }
+};
+
+const checkRefreshToken = async (refreshToken: string) => {
+  try {
+    const decodedToken: { id: string } = await jwt_decode(refreshToken);
 
     const res = await refreshTokensCollection.findOne({
       _id: ObjectId(decodedToken.id),
     });
 
-    if (!res && res.refreshToken !== refreshToken) {
-      return;
+    if (!!res && res.refreshToken !== refreshToken) {
+      return { error: true };
     }
     verifyToken(refreshToken);
     const newToken = createToken(decodedToken.id);
     const newRefreshToken = createRefreshToken(decodedToken.id);
-
     await refreshTokensCollection.updateOne({
       _id: ObjectId(decodedToken.id),
       refreshToken: newRefreshToken,
     });
 
-    const user = await userCollection.findOne({
-      _id: ObjectId(decodedToken.id),
-    });
-   
     return {
-      token:newToken,
-      refreshToken:newRefreshToken
-    }
-  } catch {
-    return;
+      verify: false,
+      error: false,
+      token: newToken,
+      refreshToken: newRefreshToken,
+    };
+  } catch (err) {
+    return {
+      error: true,
+    };
   }
 };
